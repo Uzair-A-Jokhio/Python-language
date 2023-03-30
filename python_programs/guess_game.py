06b7bf012e83d4b7dbb6d855cf27d08cf0a0be27
Uzair <uzairarifjokhio@gmail.com>
2023-03-30 16:03:27 +0500

    new program

---
+++ python_programs/guess_game.py
@@ -0,0 +1,18 @@
+print("Name of county which WON the 1992 CWC? ")
+secret_word = "pakistan"
+guess = ""
+guess_count = 0
+guess_limit = 2
+out_of_guesses = False
+
+while guess != secret_word and not(out_of_guesses):
+    if guess_count < guess_limit:
+        guess = input("Enter a guess: ")
+        guess_count += 1
+    else:
+        out_of_guesses = True
+
+if out_of_guesses:
+    print("OUT of guesses, YOU LOSE! ")
+else:
+    print("Correct!! You win!!")
